9d507b810fbedcc6e00ff420f1a919a047d3400e
tingtingj12 <ruotingj12@gmail.com>
2017-12-03 11:08:38 -0500

    Create compo.js

---
+++ compo.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const MainName = styled.h1`
+    font-size: 1.5em;
+    text-align: center;
+    color: palevioletred;
+`;
+
+export const HeaderDiv = styled.section`
+    padding: 4em;
+    background: papayawhip;
+    background-size:auto;
+    width:100vw;
+    background-color:#D0021B;
+    height:10vh;
+`;
+
+export const Slogan = styled.h2`
+    text-align: center:
+    color: white;
+`;
+
+export const Input = styled.input`
+    padding: 0.5em;
+    margin: 0.5em;
+    color: palevioletred;
+    background: papayawhip;
+    border: none;
+    border-radius: 3px;
+`;
